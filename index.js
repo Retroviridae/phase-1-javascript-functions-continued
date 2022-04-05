b840131807cf74b8db2436adfb13012f9a70a6a5
Alexander Schuessler <schuessler.alex@gmail.com>
2022-04-05 11:02:26 -0500

    commit tree 178f8074f8b4867b521ca7f43b7aae6af5d8017f

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(style="*") {
+    function innnerFunction(adjective="hard worker") {
+        return `You are ${style}a ${adjective}${style}!`
+    }
+    return innnerFunction();
+    // return `You are ${style}a ${adjective}${style}!`
+}
+
+
+// expect(emphatic).to.equal("You are *a hard worker*!")
+// expect(emphatic).to.equal("You are ||a dedicated programmer||!"
+
+console.log(saturdayFun());
+console.log(saturdayFun("bathe my dog"));
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
+console.log(wrapAdjective());
+console.log(wrapAdjective("||"));
